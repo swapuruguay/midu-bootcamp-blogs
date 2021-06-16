860f2f26d207838bb940012d45a3ec415d496ef7
Walter Sosa <sosa.tizon@gmail.com>
2021-06-16 14:15:52 -0300

    Controller aparte

--- index.js
+++ index.js
@@ -2,35 +2,29 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blogs')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
 
+//const app = require('./app')
 
-
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+.then(() => {
+  logger.info('connected to MongoDB')
+})
+.catch((error) => {
+  logger.error('error connecting to MongoDB:', error.message)
+})
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+//app.use(middleware.unknownEndpoint)
+//app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server listening on ${config.PORT}`)
 })
